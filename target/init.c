42cf78da28d8ad92619f05030fcaf700fe1e7cd4
lijuang <lijuang@codeaurora.org>
2017-12-12 14:44:32 +0800

    platform: msm8953: Add target support for sdm632 and sda632
    Add target support for sdm632 and sda632 platform.
    
    Change-Id: I4bfd71e50d40b53dfa418a0fe8b8077603a1b66f

--- target/init.c
+++ target/init.c
@@ -281,6 +281,8 @@ uint32_t target_ddr_cfg_reg()
 		case APQ8053:
 		case SDM450:
 		case SDA450:
+		case SDM632:
+		case SDA632:
 		/* SDCC HC DDR CONFIG has shifted by 4 bytes for these platform */
 			ret += 4;
 			break;
@@ -360,6 +362,8 @@ void get_vibration_type(struct qpnp_hap *config)
 		case APQ8053:
 		case SDM450:
 		case SDA450:
+		case SDM632:
+		case SDA632:
 			config->vib_type = VIB_LRA_TYPE;
 			config->hap_rate_cfg1 = QPNP_HAP_RATE_CFG1_41;
 			config->hap_rate_cfg2 = QPNP_HAP_RATE_CFG2_03;
